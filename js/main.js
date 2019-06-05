2cbb1481101f24ca9e04fb37d824008280739eb4
Vijay Ramalingam <vijayrcse@gmaail.com>
2019-06-05 11:01:39 +0530

    new changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
+const mywork =  [
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving chemistry",
+      'src': "images/example2.png",
+      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+           https://www.flickr.com/photos/ssoosay/4097410999`
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png",
+      'comment': ""
+    }
+  }
+]
 
-console.log("Loaded react dom");
+ReactDOM.render(<ExampleWork work={mywork}/>,document.getElementById('example-work'));
